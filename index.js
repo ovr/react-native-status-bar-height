7a1a287409e4fb2ac636e757c86c9a106022fba2
GauravDS <gaurav.sharma@punchh.com>
2018-01-19 03:06:33 +0530

    Feature: Use StatusBar.currentHeight

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // @flow
 
-import { Platform } from 'react-native';
+import { Platform, StatusBar } from 'react-native';
 import { ifIphoneX } from 'react-native-iphone-x-helper';
 
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? ifIphoneX(44, 20) : 0;
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? ifIphoneX(44, 20) : StatusBar.currentHeight;
 
 export function getStatusBarHeight() {
     return STATUSBAR_HEIGHT;
