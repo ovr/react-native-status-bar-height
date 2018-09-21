329b32709230f17638353e2cf761cef8bba873a4
Dmitry Patsura <talk@dmtry.me>
2018-09-21 21:52:32 +0900

    Feature: Added support to new iPhone Models

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // @flow
 import { Dimensions, Platform, StatusBar } from 'react-native';
 
-const d = Dimensions.get('window');
-const isIPhoneX = Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS && (d.height === 812 || d.width === 812);
+const X_WIDTH = 375;
+const X_HEIGHT = 812;
+
+const XSMAX_WIDTH = 414;
+const XSMAX_HEIGHT = 896;
+
+const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get('window');
+
+let isIPhoneX = false;
+
+if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
+    isIPhoneX = W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT || W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT;
+}
 
 export function getStatusBarHeight(skipAndroid: boolean = false) {
     if (Platform.OS === 'ios') {
