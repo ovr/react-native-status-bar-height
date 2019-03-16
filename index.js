0357fd677a80d3d7a092bda8aab913e7671728ea
Dmitry Patsura <talk@dmtry.me>
2019-03-16 16:58:29 +0300

    Fix: Extract flow types to definition file #10

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// @flow
 import { Dimensions, Platform, StatusBar } from 'react-native';
 
 const X_WIDTH = 375;
@@ -15,7 +14,7 @@ if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
     isIPhoneX = W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT || W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT;
 }
 
-export function getStatusBarHeight(skipAndroid: boolean = false): number {
+export function getStatusBarHeight(skipAndroid) {
     return Platform.select({
         ios: isIPhoneX ? 44 : 20,
         android: skipAndroid ? 0 : StatusBar.currentHeight,
