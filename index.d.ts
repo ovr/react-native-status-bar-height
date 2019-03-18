f95b30dc7f38a077c474ec4812694e2d3f166d61
tykus160 <tykus160@gmail.com>
2019-03-18 09:31:41 +0100

    Make useAndroid optional in TypeScript

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 
 declare module 'react-native-status-bar-height' {
-    export function getStatusBarHeight(skipAndroid: boolean): number;
+    export function getStatusBarHeight(skipAndroid?: boolean): number;
 }
