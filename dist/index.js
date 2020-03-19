6b1320e860d1d8a0b7bd4d0a92fb1a80b41712ad
Dmitry Patsura <talk@dmtry.me>
2020-03-19 13:38:04 +0300

    Feature: Improve typescript support

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,8 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 exports.__esModule = true;
-var react_native_1 = require("react-native");
-var X_WIDTH = 375;
-var X_HEIGHT = 812;
-var XSMAX_WIDTH = 414;
-var XSMAX_HEIGHT = 896;
-var _a = react_native_1.Dimensions.get('window'), W_HEIGHT = _a.height, W_WIDTH = _a.width;
-var isIPhoneX = false;
-if (react_native_1.Platform.OS === 'ios' && !react_native_1.Platform.isPad && !react_native_1.Platform.isTVOS) {
-    isIPhoneX = W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT || W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT;
-}
-var getExpoRoot = function () { return global.Expo || global.__expo || global.__exponent; };
-exports.isExpo = function () { return getExpoRoot() !== undefined; };
-function getStatusBarHeight(skipAndroid) {
-    if (skipAndroid === void 0) { skipAndroid = false; }
-    return react_native_1.Platform.select({
-        ios: isIPhoneX ? 44 : 20,
-        android: skipAndroid ? 0 : react_native_1.StatusBar.currentHeight,
-        "default": 0
-    });
-}
-exports.getStatusBarHeight = getStatusBarHeight;
+__export(require("./statusBarHeight"));
+__export(require("./withStatusBar"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
-import { Dimensions, Platform, StatusBar } from 'react-native';
 
-const X_WIDTH = 375;
-const X_HEIGHT = 812;
-
-const XSMAX_WIDTH = 414;
-const XSMAX_HEIGHT = 896;
-
-const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get('window');
-
-let isIPhoneX = false;
-
-if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
-    isIPhoneX = W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT || W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT;
-}
-
-const getExpoRoot = () => global.Expo || global.__expo || global.__exponent;
-
-export const isExpo = () => getExpoRoot() !== undefined;
-
-export function getStatusBarHeight(skipAndroid: boolean = false): number {
-    return Platform.select({
-        ios: isIPhoneX ? 44 : 20,
-        android: skipAndroid ? 0 : StatusBar.currentHeight,
-        default: 0
-    })
-}
+export * from './statusBarHeight';
+export * from './withStatusBar';
