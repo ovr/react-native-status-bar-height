f2d662eadb54b7433df65763cb1299f1e5c95086
Dmitry Patsura <talk@dmtry.me>
2020-03-23 22:26:59 +0300

    Feature: Export isIPhoneX/isIPhoneXMax/isIPhoneWithMonobrow, fix #15

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
 
 declare module 'react-native-status-bar-height' {
     export function getStatusBarHeight(skipAndroid?: boolean): number;
+    export function isIPhoneX(): boolean
+    export function isIPhoneXMax(): boolean
+    export function isIPhoneWithMonobrow(): boolean
     export function isExpo(): boolean;
 }
--- index.js
+++ index.js
@@ -8,11 +8,25 @@ const XSMAX_HEIGHT = 896;
 
 const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get('window');
 
-let isIPhoneX = false;
+let isIPhoneX_v = false;
+let isIPhoneXMax_v = false;
+let isIPhoneWithMonobrow_v = false;
 
 if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
-    isIPhoneX = W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT || W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT;
+    if (W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhoneX_v = true;
+    }
+
+    if (W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhoneXMax_v = true;
+    }
 }
+
+export const isIPhoneX = () =>  isIPhoneX_v;
+export const isIPhoneXMax = () =>  isIPhoneXMax_v;
+export const isIPhoneWithMonobrow = () => isIPhoneWithMonobrow_v;
 
 const getExpoRoot = () => global.Expo || global.__expo || global.__exponent;
 
@@ -20,7 +34,7 @@ export const isExpo = () => getExpoRoot() !== undefined;
 
 export function getStatusBarHeight(skipAndroid) {
     return Platform.select({
-        ios: isIPhoneX ? 44 : 20,
+        ios: isIPhoneWithMonobrow_v ? 44 : 20,
         android: skipAndroid ? 0 : StatusBar.currentHeight,
         default: 0
     })
