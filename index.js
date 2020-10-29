84a99f866c8a2e7b0f3c544286ae9e37ca71948a
adarsha.nayak <adarsha.nayak@curefit.com>
2020-10-29 12:48:41 +0530

    iphone 12 support

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const X_HEIGHT = 812;
 const XSMAX_WIDTH = 414;
 const XSMAX_HEIGHT = 896;
 
+const IP12_WIDTH = 390;
+const IP12_HEIGHT = 844;
+
+const IP12MAX_WIDTH = 428;
+const IP12MAX_HEIGHT = 926;
+
 const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get('window');
 
 let isIPhoneX_v = false;
@@ -21,6 +27,12 @@ if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
     if (W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT) {
         isIPhoneWithMonobrow_v = true;
         isIPhoneXMax_v = true;
+    }
+    if (W_WIDTH === IP12_WIDTH && W_HEIGHT === IP12_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+    }
+    if (W_WIDTH === IP12MAX_WIDTH && W_HEIGHT === IP12MAX_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
     }
 }
 
