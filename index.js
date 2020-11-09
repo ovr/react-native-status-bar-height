396cc63d7e433f5e745078460731fe6fbd70f555
Kamaal Farah <kamaal.f1@gmail.com>
2020-11-09 18:21:55 +0100

    Enabling support for iPhones with flat sides

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import { Dimensions, Platform, StatusBar } from 'react-native';
 
+const FLAT_SIDES_WIDTH = 390;
+const FLAT_SIDES_HEIGHT = 844;
+
+const FLAT_SIDES_PRO_MAX_WIDTH = 428
+const FLAT_SIDES_PRO_MAX_HEIGHT = 926
+
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
 
@@ -10,6 +16,8 @@ const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get('window');
 
 let isIPhoneX_v = false;
 let isIPhoneXMax_v = false;
+let isIPhone12_v = false
+let isIPhone12Max_v = false
 let isIPhoneWithMonobrow_v = false;
 
 if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
@@ -22,10 +30,22 @@ if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
         isIPhoneWithMonobrow_v = true;
         isIPhoneXMax_v = true;
     }
+
+    if (W_WIDTH === FLAT_SIDES_WIDTH && W_HEIGHT === FLAT_SIDES_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhone12_v = true;
+    }
+
+    if (W_WIDTH === FLAT_SIDES_PRO_MAX_WIDTH && W_HEIGHT === FLAT_SIDES_PRO_MAX_HEIGHT) {
+        isIPhoneWithMonobrow_v = true;
+        isIPhone12Max_v = true;
+    }
 }
 
 export const isIPhoneX = () =>  isIPhoneX_v;
 export const isIPhoneXMax = () =>  isIPhoneXMax_v;
+export const isIPhone12 = () =>  isIPhone12_v;
+export const isIPhone12Max = () =>  isIPhone12Max_v;
 export const isIPhoneWithMonobrow = () => isIPhoneWithMonobrow_v;
 
 const getExpoRoot = () => global.Expo || global.__expo || global.__exponent;
